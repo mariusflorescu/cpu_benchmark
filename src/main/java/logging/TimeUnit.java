cfcc5e29fa6c34b3ec2256acac36f6cdc2029937
GorogSergiu <sergiugorog@yahoo.com>
2021-05-19 20:53:48 +0300

    User interface done

---
+++ src/main/java/logging/TimeUnit.java
@@ -0,0 +1,28 @@
+package main.java.logging;
+
+public enum TimeUnit {
+
+	NANO, MICRO, MILLI, SEC;
+
+	/**
+	 * Converts time given in nanoseconds to another time unit
+	 * @param time Time in nanoseconds
+	 * @param unit Unit to convert to
+	 * @return Time expressed in given unit
+	 */
+	public static double toTimeUnit(long time, TimeUnit unit) {
+
+		switch (unit) {
+		case NANO:
+			return time;
+		case MICRO:
+			return time/1000.0;
+		case MILLI:
+			return time/1000000.0;
+		case SEC:
+			return time/1000000000.0;		
+		default:
+			return time;
+		}
+	}
+}
